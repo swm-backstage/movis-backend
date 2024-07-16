48ba5695f27a13a807708cc422fc1db0bbf22d71
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2024-07-16 17:49:53 +0900

    [feat] issue-#8: eventMemberId(uuid) 반환 메서드 구현
    1. clubId와 amount로 event 추정 -> 안되면 null 반환
    2. event와 name으로 eventMemberId(uuid) 추정 -> 안되면 null 반환
    
    ps. 알림 분류에서 사용할 메서드

--- src/main/java/swm/backstage/movis/domain/event/repository/EventRepository.java
+++ src/main/java/swm/backstage/movis/domain/event/repository/EventRepository.java
@@ -1,6 +1,7 @@
 package swm.backstage.movis.domain.event.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import swm.backstage.movis.domain.club.Club;
 import swm.backstage.movis.domain.event.Event;
 
@@ -10,4 +11,8 @@ import java.util.Optional;
 public interface EventRepository extends JpaRepository<Event, Long> {
     Optional<Event> findByUuid(String uuid);
     List<Event> findAllByClub(Club club);
+
+
+    @Query("SELECT e from Event e where e.club.uuid= :clubId and e.totalPaymentAmount= :amount")
+    List<Event> findByClubIdAndTotalPaymentAmount(String clubId, Long amount);
 }
