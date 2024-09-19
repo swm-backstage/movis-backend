60c14f5a677c96d68143f32a8645896da584ed3c
leestana01 <leestana01@naver.com>
2024-09-19 12:42:09 +0900

    [hotfix] id 생성 전략 추가

--- src/main/java/swm/backstage/movis/domain/invitation/VerifyCode.java
+++ src/main/java/swm/backstage/movis/domain/invitation/VerifyCode.java
@@ -1,6 +1,8 @@
 package swm.backstage.movis.domain.invitation;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -12,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class VerifyCode {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String phoneNumber;
     private String verifyCode;
