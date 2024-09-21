fad1303f02ad8c8598137911bd1ace6eee60ef80
whitej00 <wlsdka12@gmail.com>
2024-09-22 01:20:29 +0900

    [feat] issue-#59: 운영진 리스트 조회, 총무 권한 위임 구현

--- src/main/java/swm/backstage/movis/domain/club_user/repository/ClubUserRepository.java
+++ src/main/java/swm/backstage/movis/domain/club_user/repository/ClubUserRepository.java
@@ -3,9 +3,11 @@ package swm.backstage.movis.domain.club_user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import swm.backstage.movis.domain.club_user.ClubUser;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClubUserRepository extends JpaRepository<ClubUser, Long> {
 
     Optional<ClubUser> findByIdentifierAndClub_Uuid(String identifier, String clubId);
+    List<ClubUser> findAllByClub_Uuid(String clubId);
 }
