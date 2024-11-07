573bdd1150d2bba2f3b1e1f892c4ef8013553f12
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2024-11-07 14:10:24 +0900

    [faet] feature/issue-#125: 이벤트 리스트 조회에 createdAt 필드 추가

--- src/main/java/swm/backstage/movis/domain/auth/service/AuthService.java
+++ src/main/java/swm/backstage/movis/domain/auth/service/AuthService.java
@@ -48,7 +48,7 @@ public class AuthService {
     @Transactional
     public UserCreateResDto register(@Validated UserCreateReqDto userCreateReqDto) {
 
-        if (!verifyService.isVerifiedPhoneNumber(userCreateReqDto.getPhoneNo().replaceAll("-", ""))) {
+        if (!verifyService.isVerifiedPhoneNumber(userCreateReqDto.getPhoneNo())) {
 
             throw new BaseException("인증되지 않은 번호입니다 : " + userCreateReqDto.getPhoneNo(), ErrorCode.UNAUTHENTICATED_REQUEST);
         }
--- src/main/java/swm/backstage/movis/domain/event/dto/EventGetPagingListElementDto.java
+++ src/main/java/swm/backstage/movis/domain/event/dto/EventGetPagingListElementDto.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import swm.backstage.movis.domain.event.Event;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 public class EventGetPagingListElementDto {
@@ -14,9 +17,13 @@ public class EventGetPagingListElementDto {
 
     private Long balance;
 
+    private LocalDateTime createdAt;
+
+
     public EventGetPagingListElementDto(Event event) {
         this.eventId = event.getUlid();
         this.name = event.getName();
         this.balance = event.getBalance();
+        this.createdAt = event.getCreatedAt();
     }
 }
